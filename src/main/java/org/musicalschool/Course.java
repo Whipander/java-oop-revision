05b0e93f1020b77cb1eaffc60bc4f671bae42efd
Whipander <mihaja2506@gmail.com>
2025-08-05 10:55:40 +0300

    feat: update Course and Professor classes

--- src/main/java/org/musicalschool/Course.java
+++ src/main/java/org/musicalschool/Course.java
@@ -4,12 +4,25 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
 @AllArgsConstructor
 
 public class Course {
-    private String id;
     private String name;
-    private String description;
+    private Professor professor;
+    private String instrument;
+    private List<Student> registeredStudents;
+    private String day;
+
+    public void addStudent(Student student) {
+        if (registeredStudents.contains(student)) {
+            System.out.println("Student already registered");
+        }else {
+            registeredStudents.add(student);
+            System.out.println("Student registered");
+        }
+    }
 }
--- src/main/java/org/musicalschool/Professor.java
+++ src/main/java/org/musicalschool/Professor.java
@@ -4,14 +4,26 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.util.List;
+
 @Getter
 @Setter
 
 public class Professor extends Person {
     private List<String> taughtInstruments;
+    private List<Course> courses;
 
     public Professor(String nom, String prenom, String email, List<String> taughtInstruments) {
         super(nom, prenom, email);
         this.taughtInstruments = taughtInstruments;
+    }
+
+    public void teachCourse(Course course) {
+        if (courses.contains(course)) {
+            System.out.println("Course already taken");
+        } else {
+            courses.add(course);
+            System.out.println("Course added to the list");
+        }
+
     }
 }
