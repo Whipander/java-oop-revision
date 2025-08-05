5376f7bd93d88c1ed08b7fe6911f84f06f69943c
Whipander <mihaja2506@gmail.com>
2025-08-05 14:29:07 +0300

    feat: add MusicalSchool class

--- src/main/java/org/musicalschool/MusicalSchool.java
+++ src/main/java/org/musicalschool/MusicalSchool.java
@@ -1,5 +1,29 @@
 package org.musicalschool;
 
-public class MusicalSchool {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
+import java.util.Comparator;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+
+public class MusicalSchool {
+    private List<Professor> professors;
+    private List<Student> students;
+    private List<Course> courses;
+
+    public void showPlanning() {
+        courses.stream()
+                .sorted(Comparator.comparing(Course::getDay))
+                .forEach(course -> {
+                    System.out.println("Day: " + course.getDay());
+                    System.out.println("Name: " + course.getName() + " - " + course.getInstrument());
+                    System.out.println("Professor: " + course.getProfessor());
+                    System.out.println("Students: " + course.getRegisteredStudents().size());
+                });
+    }
 }
